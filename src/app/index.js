2dd35d09947ca570789b5b5792231dc04f962f7f
IvanDart1001 <IvanDart1001@outlook.com>
2018-01-19 17:09:28 +0800

    init

---
+++ src/app/index.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Created by lenovo on 2018/1/19.
+ */
+var THREE = require("three");
+/**
+ * init scene
+ */
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+/**
+ * render elements
+ */
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x0099ff, wireframe: true });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+/**
+ * rendering the scene
+ */
+(function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+    renderer.render(scene, camera);
+})();
